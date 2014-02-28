db6e933b28a91b2a1142e7ddf1e168b9080c8e2c
xian zhao <296624314@qq.com>
2014-02-28 09:53:26 +0800

    setcate

--- Lib/Action/Subshop/FoodAction.class.php
+++ Lib/Action/Subshop/FoodAction.class.php
@@ -8,8 +8,31 @@ class FoodAction extends SubshopAction{
 		$this->display();
 	}
 
+	//菜单分类列表
 	function cateList(){
 		$this->display();
+	}
+	//添加、修改菜单
+	function setCate(){
+		$id = (int)$_REQUEST['id'];
+		$cate = array();//待修改的分类信息
+		//修改模式
+		if($id > 0){
+			$map = array(
+				"id" => $id,
+				"token" => $this->token,
+				"shopid" => $this->shopid,
+			);
+			$cate = M('FoodCategory')->where($map)->find();
+		}
+		if(IS_POST){
+
+		}else{
+			if(!empty($cate)){
+				$this->assign("cate",$cate);
+			}
+			$this->display();
+		}
 	}
 	
 }
