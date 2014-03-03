a7dc69e990d0c117495c6fd1407ac942ed7cc1e5
xian zhao <296624314@qq.com>
2014-03-03 15:59:29 +0800

    weixin api

--- Lib/Action/Home/WeixinAction.class.php
+++ Lib/Action/Home/WeixinAction.class.php
@@ -1046,6 +1046,10 @@ class WeixinAction extends Action
 
     //非模块级关键词匹配
     function keyword($key){
+    	return array(
+							"$key",
+							'text'
+							);
 		//Log::write($key,Log::INFO);
 		$like['token'] = $this->token;
 		$like['keyword'] = array('like','%'.$key.'%');
