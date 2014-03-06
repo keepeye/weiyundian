5d54c1608012601d057cda96cb57efc3194d0118
xian zhao <296624314@qq.com>
2014-03-06 15:12:24 +0800

    lottery wap tpl

--- Lib/Action/Home/WeixinAction.class.php
+++ Lib/Action/Home/WeixinAction.class.php
@@ -17,7 +17,7 @@ class WeixinAction extends Action
         }
         //file_put_contents("./response.txt","----wechat\n",FILE_APPEND);
         $this->data  = $weixin->request();
-        file_put_contents("./debug.txt", time().$this->data['content']."\n",FILE_APPEND);
+        file_put_contents("./debug.txt", time().var_export($this->data,true)."\n",FILE_APPEND);
         //file_put_contents("./response.txt","----request\n",FILE_APPEND);
         $this->my    = C('site_my');
         list($content, $type) = $this->reply($this->data);
