a20bfcbc2e320cd7a87f38ecb2f2a21fced11933
xian zhao <296624314@qq.com>
2014-03-07 16:12:12 +0800

    plugmenu style=-1

--- Lib/Action/Wap/JumpAction.class.php
+++ Lib/Action/Wap/JumpAction.class.php
@@ -6,7 +6,7 @@ class JumpAction extends Action{
 		$openid = $_GET['openid'];//微信用户唯一id
 		cookie("openid",$openid);
 		cookie("wecha_id",$wecha_id);
-		dump($appurl);
+		dump(rawurldecode($appurl));
 		//redirect($appurl);
 	}
 }
