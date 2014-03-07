7cf685c5e11bf1bdeb9f546e3f0d3d13004b457d
xian zhao <296624314@qq.com>
2014-03-07 16:16:18 +0800

    plugmenu style=-1

--- Lib/Action/Wap/LotteryAction.class.php
+++ Lib/Action/Wap/LotteryAction.class.php
@@ -1,12 +1,14 @@
 <?php
 class LotteryAction extends BaseAction{
+	
 	public function index(){
 		$agent = $_SERVER['HTTP_USER_AGENT']; 
 		$token		= $this->_get('token');
-		$wecha_id	= $this->_get('wecha_id');
+		$wecha_id	= $this->_get('wecha_id') || cookie('openid');
 		if($wecha_id == ""){
 			$this->error("请通过微信进入本页面");
 		}
+		
 		$id 		= $this->_get('id');
 		$redata		= M('Lottery_record');
 		$where 		= array('token'=>$token,'wecha_id'=>$wecha_id,'lid'=>$id);
