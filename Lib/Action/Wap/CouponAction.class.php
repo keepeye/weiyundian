b1f73494fa6e98d5da63bed67a00ff3c5b7bfe8b
xian zhao <296624314@qq.com>
2014-03-10 17:39:13 +0800

    add coupon_index.html

--- Lib/Action/Wap/CouponAction.class.php
+++ Lib/Action/Wap/CouponAction.class.php
@@ -43,23 +43,11 @@ class CouponAction extends BaseAction{
 	
 	//中奖后填写信息
 	public function add(){
-		 if($_POST['action'] ==  'add' ){
-			$lid 				= $this->_post('lid');
-			$wechaid 			= $this->_post('wechaid');
-			$data['sn']			= $this->_post('sncode');
-			$data['phone'] 		= $this->_post('tel');
-			$data['prize']		= $this->_post('winprize');
-			$data['wecha_name'] = $this->_post('wxname');
-			$data['time']		= time(); 
-			$data['islottery']	= 1;
-			$data['usenums']	= 1;			
-
-			$rollback = M('Lottery_record')->where(array('lid'=> $lid,
-				'wecha_id'=>$wechaid))->save($data);
-			
-			echo'{"success":1,"msg":"恭喜！尊敬的<font color=red>'.$data['wecha_name'].'</font>请您保持手机通畅！你的领奖序号:<font color=red>'.$data['sn'].'</font>"}';
-			exit;	
-		}
+		$return = array(
+				"status"=>"0",
+				"info"=>"糟糕，被人抢先一步，优惠券被领完了！"
+			);
+		$this->ajaxReturn($return);
 	}
 }
 	
