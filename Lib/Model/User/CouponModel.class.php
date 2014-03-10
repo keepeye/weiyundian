85ef042a20c0984475170033deb6ddda3281db42
xian zhao <296624314@qq.com>
2014-03-10 17:07:33 +0800

    add coupon_index.html

--- Lib/Model/User/CouponModel.class.php
+++ Lib/Model/User/CouponModel.class.php
@@ -3,11 +3,14 @@ class CouponModel extends Model{
 
 	protected $_validate = array(
 			array('keyword','require','关键词不能为空',1),
+			array('keyword','1,30','关键词最多10个字',1,'length'),
 			array('title','require','活动标题不能为空',1),
+			array('title','1,65','标题太长，最多20个汉字',1,'length'),
 			array('start_time','require','开始时间不能为空',1),			
 			array('end_time','require','结束时间不能为空',1),
 			array('end_time', 'checkdate', '结束时间不能小于开始时间',Model::MUST_VALIDATE,'callback',3),
 			array('info','require','活动说明不能为空',1),
+			array('info','1,600','介绍最多200个字',1,'length'),
 			array('num', 'checknum', '优惠券必须是大于0的数字',Model::MUST_VALIDATE,'callback',3),
 	 );
 	protected $_auto = array (    
