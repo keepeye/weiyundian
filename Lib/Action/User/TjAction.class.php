a8a3f36f229086642c30863bc58b9bc47eea0e3b
xian zhao <296624314@qq.com>
2014-03-13 09:54:50 +0800

    新的统计接口

---
+++ Lib/Action/User/TjAction.class.php
@@ -0,0 +1,18 @@
+<?php
+//访问、转发统计查看
+class TjAction extends TongjiBaseAction{
+	//概况，只显示统计总数
+	function index(){
+
+	}
+
+	//详细统计
+	function details(){
+
+	}
+	
+	//文档具体统计,
+	function detail(){
+
+	}
+}
