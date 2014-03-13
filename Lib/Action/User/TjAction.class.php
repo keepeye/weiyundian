c69cd2279b2de8723cdaafe640c01dadc5af65b9
xian zhao <296624314@qq.com>
2014-03-13 14:56:49 +0800

    后台统计模块开发

--- Lib/Action/User/TjAction.class.php
+++ Lib/Action/User/TjAction.class.php
@@ -14,7 +14,7 @@ class TjAction extends UserAction{
 		$list = $this->_model->field("concat(year,'/',month,'/',day) as day,SUM(shares) as shares,SUM(clicks) as clicks")->where(array("token"=>$this->_token))->group("year,month,day")->limit(0,10)->order("id ASC")->select();//10天内的数据
 		$data['day'] = array_column($list,"day");
 		$data['shares'] = array_column($list,"shares");
-		$data['clicks'] = array_column($list,"clicks");
+		//$data['clicks'] = array_column($list,"clicks");
 		
 		 $this->assign("data",$data);
 		 $this->display();
