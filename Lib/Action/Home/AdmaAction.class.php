40d62ce8c8c12da106c8f6d7e90e5f488a71d484
xian zhao <296624314@qq.com>
2014-03-14 16:37:42 +0800

    wap下coupon加入统计

--- Lib/Action/Home/AdmaAction.class.php
+++ Lib/Action/Home/AdmaAction.class.php
@@ -5,7 +5,7 @@ class AdmaAction extends BaseAction{
 		if($this->_get('token')!=false){
 			$adma=M('Adma')->where(array('token'=>$this->_get('token')))->find();
 			if($adma==false){
-				$this->error('不在的宣传页',U('Home/Index/index'));
+				$this->error('你好本页面禁止从外部分享链接直接访问',U('Home/Index/index'));
 			}else{
 				if($adma['redirect']!=""){
 					redirect($adma['redirect']);//如果设置了外链，则直接跳转到外链页面
