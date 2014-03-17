3ca35876aa40e57b5e15b98521a03b353b753e9d
xian zhao <296624314@qq.com>
2014-03-17 12:40:41 +0800

    user/Coupon/index修改

--- Lib/Action/User/CouponAction.class.php
+++ Lib/Action/User/CouponAction.class.php
@@ -6,22 +6,14 @@ class CouponAction extends UserAction{
 		$this->token = session('token');//保存token信息
 		
 	}
+	//优惠券活动列表
 	public function index(){
-	//dump(session('token'));
-		if(session('gid')==1){
-			$this->error('vip0无法使用抽奖活动,请充值后再使用',U('User/Index/index'));
-		}
-		$user=M('Users')->field('gid,activitynum')->where(array('id'=>session('uid')))->find();
-		$group=M('User_group')->where(array('id'=>$user['gid']))->find();
-		$this->assign('group',$group);
-		$this->assign('activitynum',$user['activitynum']);
-		$list=M('Lottery')->where(array('token'=>session('token'),'type'=>3))->select();
-		//echo M('Lottery')->getLastSql();
-		$this->assign('count',M('Lottery')->where(array('token'=>session('token'),'type'=>3))->count());
+		//读取活动列表
+		$list=M('Coupon')->where(array('token'=>session('token')))->select();
 		$this->assign('list',$list);
 		$this->display();
-	
 	}
+
 	//添加修改活动信息
 	public function set(){
 		$CouponM = D('Coupon');//实例化模型
@@ -62,23 +54,23 @@ class CouponAction extends UserAction{
 
 
 	}
-	public function sn(){
-		if(session('gid')==1){
-			$this->error('vip0无法使用抽奖活动,请充值后再使用',U('User/Index/index'));
-		}
-		$id=$this->_get('id');
-		$data=M('Lottery')->where(array('token'=>session('token'),'id'=>$id,'type'=>3))->find();
-		$record=M('Lottery_record')->where('token="'.session('token').'" and lid='.$id.' and sn!=""')->select();
-		$recordcount=M('Lottery_record')->where('token="'.session('token').'" and lid='.$id.' and sn!=""')->count();
-		$datacount=$data['fistnums']+$data['secondnums']+$data['thirdnums'];
-		$this->assign('datacount',$datacount);
-		$this->assign('recordcount',$recordcount);
-		$this->assign('record',$record);
+	// public function sn(){
+	// 	if(session('gid')==1){
+	// 		$this->error('vip0无法使用抽奖活动,请充值后再使用',U('User/Index/index'));
+	// 	}
+	// 	$id=$this->_get('id');
+	// 	$data=M('Lottery')->where(array('token'=>session('token'),'id'=>$id,'type'=>3))->find();
+	// 	$record=M('Lottery_record')->where('token="'.session('token').'" and lid='.$id.' and sn!=""')->select();
+	// 	$recordcount=M('Lottery_record')->where('token="'.session('token').'" and lid='.$id.' and sn!=""')->count();
+	// 	$datacount=$data['fistnums']+$data['secondnums']+$data['thirdnums'];
+	// 	$this->assign('datacount',$datacount);
+	// 	$this->assign('recordcount',$recordcount);
+	// 	$this->assign('record',$record);
 	
-		$this->display();
+	// 	$this->display();
 	
 	
-	}
+	// }
 	// public function add(){
 	// 	if(session('gid')==1){
 	// 		$this->error('vip0无法使用优惠券管理,请充值后再使用',U('User/Index/index'));
@@ -123,75 +115,75 @@ class CouponAction extends UserAction{
 	// 		$this->display();
 	// 	}
 	// }
-	public function setinc(){
-		if(session('gid')==1){
-			$this->error('vip0无法开启活动,请充值后再使用',U('User/Index/index'));
-		}
-		$id=$this->_get('id');
-		$where=array('id'=>$id,'token'=>session('token'));
-		$check=M('Lottery')->where($where)->find();
-		if($check==false)$this->error('非法操作');
-		$data=M('Lottery')->where($where)->setInc('status');
-		if($data!=false){
-			$this->success('恭喜你,活动已经开始');
-		}else{
-			$this->error('服务器繁忙,请稍候再试');
-		}
+	// public function setinc(){
+	// 	if(session('gid')==1){
+	// 		$this->error('vip0无法开启活动,请充值后再使用',U('User/Index/index'));
+	// 	}
+	// 	$id=$this->_get('id');
+	// 	$where=array('id'=>$id,'token'=>session('token'));
+	// 	$check=M('Lottery')->where($where)->find();
+	// 	if($check==false)$this->error('非法操作');
+	// 	$data=M('Lottery')->where($where)->setInc('status');
+	// 	if($data!=false){
+	// 		$this->success('恭喜你,活动已经开始');
+	// 	}else{
+	// 		$this->error('服务器繁忙,请稍候再试');
+	// 	}
 
-	}
-	public function setdes(){
-		$id=$this->_get('id');
-		$where=array('id'=>$id,'token'=>session('token'));
-		$check=M('Lottery')->where($where)->find();
-		if($check==false)$this->error('非法操作');
-		$data=M('Lottery')->where($where)->setDec('status');
-		if($data!=false){
-			$this->success('活动已经结束');
-		}else{
-			$this->error('服务器繁忙,请稍候再试');
-		}
+	// }
+	// public function setdes(){
+	// 	$id=$this->_get('id');
+	// 	$where=array('id'=>$id,'token'=>session('token'));
+	// 	$check=M('Lottery')->where($where)->find();
+	// 	if($check==false)$this->error('非法操作');
+	// 	$data=M('Lottery')->where($where)->setDec('status');
+	// 	if($data!=false){
+	// 		$this->success('活动已经结束');
+	// 	}else{
+	// 		$this->error('服务器繁忙,请稍候再试');
+	// 	}
 	
-	}
-	public function edit(){
-		if(IS_POST){
-			$data=D('Lottery');
-			$_POST['id']=$this->_get('id');
-			$_POST['token']=session('token');
-			$_POST['statdate']=strtotime($_POST['statdate']);
-			$_POST['enddate']=strtotime($_POST['enddate']);
-			if($_POST['enddate'] < $_POST['statdate']){
-				$this->error('结束时间不能小于开始时间');
-			}else{
-				$where=array('id'=>$_POST['id'],'token'=>$_POST['token'],'type'=>3);
-				$check=$data->where($where)->find();
-				if($check==false)$this->error('非法操作');
-					if($data->where($where)->save($_POST)){
-						$data1['pid']=$_POST['id'];
-						$data1['module']='Lottery';
-						$data1['token']=session('token');
-						$da['keyword']=$_POST['keyword'];
-						M('Keyword')->where($data1)->save($da);
-						$this->success('修改成功',U('Coupon/index',array('token'=>session('token'))));
-					}else{
-						$this->error('操作失败');
-					}
+	// }
+	// public function edit(){
+	// 	if(IS_POST){
+	// 		$data=D('Lottery');
+	// 		$_POST['id']=$this->_get('id');
+	// 		$_POST['token']=session('token');
+	// 		$_POST['statdate']=strtotime($_POST['statdate']);
+	// 		$_POST['enddate']=strtotime($_POST['enddate']);
+	// 		if($_POST['enddate'] < $_POST['statdate']){
+	// 			$this->error('结束时间不能小于开始时间');
+	// 		}else{
+	// 			$where=array('id'=>$_POST['id'],'token'=>$_POST['token'],'type'=>3);
+	// 			$check=$data->where($where)->find();
+	// 			if($check==false)$this->error('非法操作');
+	// 				if($data->where($where)->save($_POST)){
+	// 					$data1['pid']=$_POST['id'];
+	// 					$data1['module']='Lottery';
+	// 					$data1['token']=session('token');
+	// 					$da['keyword']=$_POST['keyword'];
+	// 					M('Keyword')->where($data1)->save($da);
+	// 					$this->success('修改成功',U('Coupon/index',array('token'=>session('token'))));
+	// 				}else{
+	// 					$this->error('操作失败');
+	// 				}
 				
-			}
+	// 		}
 
 			
-		}else{
-			$id=$this->_get('id');
-			$where=array('id'=>$id,'token'=>session('token'));
-			$data=M('Lottery');
-			$check=$data->where($where)->find();
-			if($check==false)$this->error('非法操作');
-			$lottery=$data->where($where)->find();		
-			$this->assign('vo',$lottery);
-			//dump($_POST);
-			$this->display('add');
-		}
+	// 	}else{
+	// 		$id=$this->_get('id');
+	// 		$where=array('id'=>$id,'token'=>session('token'));
+	// 		$data=M('Lottery');
+	// 		$check=$data->where($where)->find();
+	// 		if($check==false)$this->error('非法操作');
+	// 		$lottery=$data->where($where)->find();		
+	// 		$this->assign('vo',$lottery);
+	// 		//dump($_POST);
+	// 		$this->display('add');
+	// 	}
 	
-	}
+	// }
 	public function del(){
 		$id=$this->_get('id');
 		$where=array('id'=>$id,'token'=>session('token'));
