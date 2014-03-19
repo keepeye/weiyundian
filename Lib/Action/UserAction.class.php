ff724649956d52b338e3ef8dfea1beb09a707a59
xian zhao <296624314@qq.com>
2014-03-19 15:45:12 +0800

    初步完成子账户登录入口

--- Lib/Action/UserAction.class.php
+++ Lib/Action/UserAction.class.php
@@ -30,6 +30,7 @@ class UserAction extends BaseAction{
 				$sub_access = M('wxuser_sub_access')->where(array("uid"=>session('sub_uid')))->find();//读取权限信息
 				$rules = unserialize($sub_access);//反序列化权限数组并做小写转换
 				if(GROUP_NAME != "User" || !isset($rules[MODULE_NAME])){
+					dump($rules);
 					$this->error("你没有权限查看该功能");
 				}
 				//检查当前操作是否被禁止
