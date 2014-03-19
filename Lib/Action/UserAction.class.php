913049cdab79dc63e8004c22f9da25be48b3aabd
xian zhao <296624314@qq.com>
2014-03-19 15:34:33 +0800

    初步完成子账户登录入口

--- Lib/Action/UserAction.class.php
+++ Lib/Action/UserAction.class.php
@@ -1,11 +1,12 @@
 <?php
 class UserAction extends BaseAction{
-	protected $trade;
-	protected $token;
+	protected $trade;//行业
+	protected $token;//微信token
 	protected function _initialize(){
 		parent::_initialize();
-		$this->token = session('token');
+		
 		$userinfo=M('User_group')->where(array('id'=>session('gid')))->find();
+		$this->assign('userinfo',$userinfo);
 		$users=M('Users')->where(array('id'=>$_SESSION['uid']))->find();
 		$this->assign('thisUser',$users);
 		//dump($users);
@@ -18,15 +19,34 @@ class UserAction extends BaseAction{
 				$this->error('您的帐号已经到期，请充值后再使用');
 			}
 		}
-		$wecha=M('Wxuser')->field('wxname,wxid,headerpic,weixin')->where(array('token'=>$this->token,'uid'=>session('uid')))->find();
-		$this->assign('wecha',$wecha);
-		$this->assign('token',$this->token);
-		
-		$this->assign('userinfo',$userinfo);
+		//尝试从session中获取token，并查询wxuser详细信息
+		$this->token = session('token');
+		if($this->token){
+			$wecha=M('Wxuser')->field('wxname,wxid,headerpic,weixin')->where(array('token'=>$this->token,'uid'=>session('uid')))->find();
+			$this->assign('wecha',$wecha);
+			$this->assign('token',$this->token);
+			//判断是否子账号，并进行权限判定，主意排除Tongji模块，避免无法访问首页
+			if(session('is_wxsub') && !(GROUP_NAME=="User" && MODULE_NAME=="Tongji")){
+				$sub_access = M('wxuser_sub_access')->where(array("uid"=>session('sub_uid')))->find();//读取权限信息
+				$rules = array_map("strtolower",unserialize($sub_access));//反序列化权限数组并做小写转换
+				if(GROUP_NAME != "User" || !isset($rules[MODULE_NAME])){
+					$this->error("你没有权限查看该功能");
+				}
+				//检查当前操作是否被禁止
+				if(in_array(strtolower(ACTION_NAME),$rules)){
+					$this->error("你没有权限进行此项操作");
+				}
+			}
+		}
+		//判断用户是否登录
 		if(session('uid')==false){
 			$this->redirect('Home/Index/login');
 		}
+
+		//所属行业
 		$this->trade = $users['trade'];
 		$this->assign('trade',$this->trade);
+
+
 	}
 }
