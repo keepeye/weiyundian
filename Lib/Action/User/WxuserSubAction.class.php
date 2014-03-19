a209e39cac8c8f5c116d75d26ba9521c2d2c4887
xian zhao <296624314@qq.com>
2014-03-19 10:06:25 +0800

    创建WxuserSub/index模板

--- Lib/Action/User/WxuserSubAction.class.php
+++ Lib/Action/User/WxuserSubAction.class.php
@@ -4,7 +4,7 @@ class WxuserSubAction extends UserAction{
 	function index(){
 		//读取子账户列表
 		$list = M('WxuserSub')->where(array("token"=>$this->token))->select();
-		dump($list);
-		//$this->display();
+		$this->assign("list",$list);
+		$this->display();
 	}
 }
