30d486799979abd2d7c871a6f08c64d9bfe2128f
xian zhao <296624314@qq.com>
2014-03-20 15:22:18 +0800

    增加事件统计功能

--- Lib/Action/User/TjAction.class.php
+++ Lib/Action/User/TjAction.class.php
@@ -95,7 +95,7 @@ class TjAction extends UserAction{
 		}
 		$map['lasttime'] = array("between",array($stime,$etime));//确定时间区间
 
-		$list = M('Tongji')->field("title,type,SUM(clicks) as clicks,SUM('shares') as shares")->where($map)->group("pid")->select();
+		$list = M('Tongji')->field("title,type,SUM(clicks) as clicks,SUM(shares) as shares")->where($map)->group("pid")->select();
 		$this->assign("list",$list);
 		dump(M('Tongji')->getLastSql());
 		$this->display();
