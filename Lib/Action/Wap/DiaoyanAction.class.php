03cba47e07f256efaa798141517a36d167c3202c
xian zhao <296624314@qq.com>
2014-03-21 15:49:24 +0800

    开发调研模块

--- Lib/Action/Wap/DiaoyanAction.class.php
+++ Lib/Action/Wap/DiaoyanAction.class.php
@@ -69,7 +69,7 @@ class DiaoyanAction extends BaseAction {
 	function submit(){
 		if(IS_POST){
 			$diaoyan_id = I('diaoyan_id','0','intval');
-			$results = $_POST['results'];
+			$results = $_POST['results'];//结果数组
 			dump($results);exit;
 			//取题库信息
 			$tiku_list = M('DiaoyanTiku')->where(array("token"=>$this->token,"diaoyan_id"=>$diaoyan_id))->limit(0,10)->select();//获取题库列表
@@ -78,12 +78,27 @@ class DiaoyanAction extends BaseAction {
 				$tiku_ids[] = $v['id'];
 			}
 			unset($v);
-			$options = M('DiaoyanTikuOption')->where(array("tiku_id"=>array("in",$tiku_ids)))->select();//读取选项表
-			$option_list = array();
-			foreach($options as $v){
-				$option_list[$v['tiku_id']][]=$v;
+			//开始写入记录
+			foreach ($results as $result) {
+				if(!in_array($result['tiku_id'],$tiku_ids)){
+					continue;//如果题库id不属于当前活动，则跳过本次循环
+				}
+				//循环插入record
+				foreach($result['option_ids'] as $option_id){
+					$data = array(
+						"wecha_id"=>$this->wecha_id,
+						"tiku_id"=>$result['tiku_id'],
+						"diaoyan_id"=>$diaoyan_id,
+						"option_id"=>$option_id
+						);
+					M('DiaoyanRecord')->add($data);
+					unset($data,$option_id);//清空数据
+				}
+				unset($result);
 			}
-			unset($v);
+			$this->success("ok");
+		}else{
+			$this->error("非法提交[04]");
 		}
 	}
 	//检测合法性
