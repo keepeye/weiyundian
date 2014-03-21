4cb10dabde5fd8d0507495ff12f898718874ec29
xian zhao <296624314@qq.com>
2014-03-21 17:19:16 +0800

    开发调研模块

--- Lib/Action/Wap/OauthAction.class.php
+++ Lib/Action/Wap/OauthAction.class.php
@@ -9,9 +9,13 @@ class OauthAction extends Action{
 		$token = I('token');//商户token
 		$referer = I('referer','','htmlspecialchars_decode');//来路
 		
-		cookie("auth_referer",$referer,300);//用cookie临时存储来路url
+		cookie("referer",$referer,300);//用cookie临时存储来路url
 
 		$appsec = M('DiymenSet')->where(array("token"=>$token))->find();
+
+		cookie("appid",$appsec['appid'],300);
+		cookie("secret",$appsec['appsecret'],300);
+
 		$redirect_uri = C('site_url').'/'.U("Wap/Oauth/auth");//回调页面
 
 		$redirect_uri = rawurlencode($redirect_uri);//urlencode处理
@@ -22,6 +26,22 @@ class OauthAction extends Action{
 	//第一步的回调地址，从这里使用code获取access，进而登录用户信息
 	function auth(){
 		$code = I('code');
-		dump(cookie("auth_referer"));
+		$appid = cookie("appid");
+		$secret = cookie("secret");
+		$url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid={$appid}&secret={$secret}&code={$code}&grant_type=authorization_code";
+		$ch = curl_init();
+		curl_setopt($ch,CURLOPT_URL,$url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+		$data =  curl_exec($ch);
+		curl_close($ch);
+		dump($data);
+		$this->clearCookie();
+	}
+
+	//清空临时cookie
+	function clearCookie(){
+		cookie('referer',null);
+		cookie('appid',null);
+		cookie('secret',null);
 	}
 }
