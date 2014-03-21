3c07ff225b43e7cd27ebd561ad64954aa5fc5934
xian zhao <296624314@qq.com>
2014-03-21 09:52:48 +0800

    开发调研模块

--- Lib/Action/User/DiaoyanAction.class.php
+++ Lib/Action/User/DiaoyanAction.class.php
@@ -16,7 +16,39 @@ class DiaoyanAction extends UserAction {
 	}
 	//设置调研
 	function setDiaoyan(){
-
+		$id = I('id','0','intval');
+		$isNew = $id > 0?false:true;//判断是否添加操作
+		if(!$isNew){
+			$diaoyan = M('Diaoyan')->where(array('id'=>$id,'token'=>$this->_token))->find();//更新模式下检查活动是否存在
+			if(!$diaoyan){
+				$this->error("活动不存在");
+			}
+			$diaoyan['keyword'] = M('Keyword')->where(array("pid"=>$id,"module"=>"Diaoyan","token"=>$this->_token))->getField("keyword");//获取关键词
+		}
+		//判断是否提交表单
+		if(!IS_POST){
+			$this->assign("diaoyan",$diaoyan);
+			$this->display();//显示视图
+		}else{
+			
+			$_POST['token'] = $this->_token;//将token添加到表单数据中，避免丢失
+			$_POST['id'] = $id;//同上
+			if(M('Diaoyan')->create()){
+				$re = $isNew?M('Diaoyan')->add():M('Diaoyan')->save();
+				if($re !== false){
+					$pid = $isNew?$re:$id;//获取主键
+					if($pid > 0){
+						//更新关键词
+						D('Keyword')->setKeyword(I("post.keyword"),$pid,$this->_token,"Diaoyan",1);
+					}
+					$this->success("保存成功");
+				}else{
+					$this->error("保存数据失败:".M('Diaoyan')->getLastSql());
+				}
+			}else{
+				$this->error(M('Diaoyan')->getError());
+			}
+		}
 	}
 	//题库
 	function questionList(){
