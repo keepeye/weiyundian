96b61b12b834aa7ecb98e70a264fa798baf57b41
xian zhao <296624314@qq.com>
2014-03-27 10:47:08 +0800

    调研题库增加批量导入

--- Lib/Action/User/DiaoyanAction.class.php
+++ Lib/Action/User/DiaoyanAction.class.php
@@ -113,7 +113,6 @@ class DiaoyanAction extends UserAction {
 			if($multi_add = I('multi_add')){
 				$multi_options = explode("\n",$multi_add);//一行一个
 				$newoptions = array_merge((array)$newoptions,$multi_options);
-				dump($newoptions);exit;
 			}
 			//进行一系列检测，时间关系就不使用模型自动验证了
 			if((count($oldoptions) + count($newoptions))>100){
