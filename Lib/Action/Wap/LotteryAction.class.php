b98404f54eb9a1484d3b0e611d6b68a07b408cfa
xian zhao <296624314@qq.com>
2014-04-01 13:41:17 +0800

    公众号增加网页授权高级接口选择

--- Lib/Action/Wap/LotteryAction.class.php
+++ Lib/Action/Wap/LotteryAction.class.php
@@ -28,7 +28,13 @@ class LotteryAction extends BaseAction{
 
 		//检测当前访问的合法性
 		if($wecha_id == "" || md5($token.$wecha_id.C('safe_key'))!=$wxsign){
-			$this->redirect("Home/Adma/index?token=".$token);
+			$wxuser = M('Wxuser')->field('has_oauth')->where(array('token'=>$token))->find();
+			if($wxuser && $wxuser['has_oauth']=="1"){
+				redirect(U("Wap/Oauth/getCode",array("token"=>$this->token,"referer"=>rawurlencode('http://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'].'?'.$_SERVER['QUERY_STRING']))));
+			}else{
+				$this->redirect("Home/Adma/index?token=".$token);
+			}
+			
 		}
 		$this->assign("token",$token);
 		$this->assign("wecha_id",$wecha_id);
