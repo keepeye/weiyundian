e4375c02d6e8a9def9f40e81923b3dca7964df96
xian zhao <296624314@qq.com>
2014-04-04 13:38:03 +0800

    统计概况倒叙

--- Lib/Action/User/TjAction.class.php
+++ Lib/Action/User/TjAction.class.php
@@ -18,7 +18,7 @@ class TjAction extends UserAction{
 		$end_time = $end_date?strtotime($end_date):strtotime(date("Y-m-d",time()))+86400;
 		$start_date = I('start_date','','trim');
 		$start_time = $start_date?strtotime($start_date):($end_time-86400*30);
-		echo $start_time;
+		echo $end_time-86400*30;
 		$list = $this->_model->field("concat(year,'/',month,'/',day) as day,SUM(shares) as shares,SUM(clicks) as clicks")->where(array("token"=>$this->_token,"lasttime"=>array("in",array($start_time,$end_time))))->group("year,month,day")->order("id ASC")->select();
 		$data['day'] = array_column($list,"day");
 		$data['shares'] = array_column($list,"shares");
