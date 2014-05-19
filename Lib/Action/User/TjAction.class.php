96d078250545524383b94710d091eeedfa21fd3f
xian zhao <296624314@qq.com>
2014-05-19 14:46:46 +0800

    nothing

--- Lib/Action/User/TjAction.class.php
+++ Lib/Action/User/TjAction.class.php
@@ -99,8 +99,7 @@ class TjAction extends UserAction{
 		}
 		$map['lasttime'] = array("between",array($stime,$etime));//确定时间区间
 
-		$list = M('Tongji')->field("title,type,SUM(clicks) as clicks,SUM(shares) as shares")->where($map)->group("pid")->order('id desc')->select();
-		echo M()->getLastSql();
+		$list = M('Tongji')->field("id,title,type,SUM(clicks) as clicks,SUM(shares) as shares")->where($map)->group("pid")->order('id desc')->select();
 		$this->assign("list",$list);
 		$this->display();
 	}
