9170db8e630484610f15fda19ebdee3facf1cc04
xian zhao <296624314@qq.com>
2014-05-22 11:37:44 +0800

    gitpush

--- Lib/Action/User/LotteryAction.class.php
+++ Lib/Action/User/LotteryAction.class.php
@@ -53,12 +53,13 @@ class LotteryAction extends UserAction{
 		}
 		
 		$recordcount=M('Lottery_record')->where($map)->count();//中奖总数
-		echo M()->getLastSql();
+
 		//分页
 		$count      = $recordcount;
 		$Page       = new Page($count,20);
 		$pagestr       = $Page->show();
 		$record=M('Lottery_record')->where($map)->order('`time` desc')->limit($Page->firstRow.','.$Page->listRows)->select();//中奖列表
+		echo M()->getLastSql();
 		$this->assign('pagestr',$pagestr);
 		//分页结束
 		$datacount=$data['fistnums']+$data['secondnums']+$data['thirdnums']+$data['fournums']+$data['fivenums']+$data['sixnums'];//奖品总数
