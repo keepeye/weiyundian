2d0f229e939531eaf1eae31427e830246c3e9cf1
xian zhao <296624314@qq.com>
2014-05-28 15:30:47 +0800

    gitpush

--- Lib/Action/Wap/SelfformAction.class.php
+++ Lib/Action/Wap/SelfformAction.class.php
@@ -79,7 +79,7 @@ class SelfformAction extends BaseAction{
 			}
 			$this->wecha_id = $cookie_data['wecha_id'];//伪造的wecha_id
 			$submitted=0;
-			
+			echo $this->wecha_id;
 			//判断是否提交过信息了
 			$submitInfo=$this->selfform_value_model->where(array('wecha_id'=>$this->wecha_id,'formid'=>$thisForm['id']))->find();
 			if ($submitInfo){
