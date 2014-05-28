581fcafe9aba2dc26efad6210ea47b6afef63ba7
xian zhao <296624314@qq.com>
2014-05-28 15:22:52 +0800

    gitpush

--- Lib/Action/Wap/SelfformAction.class.php
+++ Lib/Action/Wap/SelfformAction.class.php
@@ -78,24 +78,24 @@ class SelfformAction extends BaseAction{
 				cookie($cookie_key,$cookie_data);				
 			}
 			$this->wecha_id = $cookie_data['wecha_id'];//伪造的wecha_id
-			// $submitted=0;
+			$submitted=0;
 			
-			//判断是否提交过信息了
-			// $submitInfo=$this->selfform_value_model->where(array('wecha_id'=>$this->wecha_id,'formid'=>$thisForm['id']))->find();
-			// if ($submitInfo){
-			// 	$info=unserialize($submitInfo['values']);
-			// 	if ($info){
-			// 		foreach ($info as $k=>$v){
-			// 			$info[$k]=array('displayname'=>$listByKey[$k]['displayname'],'value'=>$v);
-			// 		}
-			// 	}
-			// 	$this->assign('submitInfo',$info);
-			// 	//$submitted=1;
-			// 	//二维码图片
-			// 	//$imgSrc=generateQRfromGoogle(C('site_url').'/index.php?g=Wap&m=Selfform&a=submitInfo&token='.$this->token.'&wecha_id='.$this->wecha_id.'&id='.$thisForm['id']);
+			判断是否提交过信息了
+			$submitInfo=$this->selfform_value_model->where(array('wecha_id'=>$this->wecha_id,'formid'=>$thisForm['id']))->find();
+			if ($submitInfo){
+				$info=unserialize($submitInfo['values']);
+				if ($info){
+					foreach ($info as $k=>$v){
+						$info[$k]=array('displayname'=>$listByKey[$k]['displayname'],'value'=>$v);
+					}
+				}
+				$this->assign('submitInfo',$info);
+				//$submitted=1;
+				//二维码图片
+				//$imgSrc=generateQRfromGoogle(C('site_url').'/index.php?g=Wap&m=Selfform&a=submitInfo&token='.$this->token.'&wecha_id='.$this->wecha_id.'&id='.$thisForm['id']);
 				
-			// 	//$this->assign('imgSrc',$imgSrc);
-			// }
+				//$this->assign('imgSrc',$imgSrc);
+			}
 			
 			$this->assign('company',$company);
 			$this->assign('submitted',$submitted);
