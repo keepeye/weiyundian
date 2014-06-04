fb3f3025ae3902d3c2e2f62bd8d3c3031271e514
xian zhao <296624314@qq.com>
2014-06-04 11:08:09 +0800

    gitpush

--- Lib/Action/Wap/LotteryAction.class.php
+++ Lib/Action/Wap/LotteryAction.class.php
@@ -35,7 +35,11 @@ class LotteryAction extends BaseAction{
 			if($wxuser && $wxuser['has_oauth']=="1"){
 				redirect(U("Wap/Oauth/getCode",array("token"=>$this->token,"referer"=>rawurlencode('http://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'].'?'.$_SERVER['QUERY_STRING']))));
 			}else{
-				$this->redirect("Home/Adma/index?token=".$token);
+				if(!empty($Lottery['redirect'])){
+					redirect($Lottery['redirect']);
+				}else{
+					$this->redirect("Home/Adma/index?token=".$token);
+				}
 			}
 			//$this->redirect("Home/Adma/index?token=".$token);
 		}
