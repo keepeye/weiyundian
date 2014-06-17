4f6dcce25afefa181c34ae1bd3fa71ee248eea11
xian zhao <296624314@qq.com>
2014-06-17 14:10:48 +0800

    gitpush

--- Lib/Action/Wap/GuajiangAction.class.php
+++ Lib/Action/Wap/GuajiangAction.class.php
@@ -28,6 +28,7 @@ class GuajiangAction extends WapAction{
 
 		//检测当前访问的合法性
 		if(!$this->checkWxsign()){
+			dump(encrypt(I('fromuser',''),"D",C('safe_key')));exit;
 			// $wxuser = M('Wxuser')->field('has_oauth')->where(array('token'=>$token))->find();
 			// if($wxuser && $wxuser['has_oauth']=="1"){
 			// 	redirect(U("Wap/Oauth/getCode",array("token"=>$this->token,"referer"=>rawurlencode('http://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'].'?'.$_SERVER['QUERY_STRING']))));
