54b8b14d8b501daaa98e33ee4cc9c2fdf2c8fdf2
xian zhao <296624314@qq.com>
2014-06-18 15:28:56 +0800

    gitpush

--- Lib/Action/User/GiftAction.class.php
+++ Lib/Action/User/GiftAction.class.php
@@ -19,4 +19,38 @@ class GiftAction extends UserAction
 		$this->display();
 	}
 
+	//添加礼品
+	function add()
+	{
+		$this->display("edit");
+	}
+
+	//编辑礼品
+	function edit()
+	{
+		$id = I('id',0);
+		$m = M('Gift');
+		if( ! $id || ! ($gift = $m->where(array("id"=>$id,"token"=>$this->token))->find()))
+		{
+			$this->error("礼品不存在");
+		}
+
+		$this->assign("gift",$gift);
+		$this->display();
+	}
+	
+	//更新数据
+	function update()
+	{
+
+	}
+
+	//删除礼品
+	function delete()
+	{
+		$id = I('id',0);
+		$m = M('Gift');
+		$m->where(array("id"=>$id,"token"=>$this->token))->delete();
+		$this->success("删除成功");
+	}
 }
