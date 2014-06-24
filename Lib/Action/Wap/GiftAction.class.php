7dd0ab77a22fa6e836280be1640bb2918b394a1b
xian zhao <296624314@qq.com>
2014-06-24 10:51:01 +0800

    gitpush

--- Lib/Action/Wap/GiftAction.class.php
+++ Lib/Action/Wap/GiftAction.class.php
@@ -2,6 +2,8 @@
 class GiftAction extends WapAction {
 	public $token;
 	public $wecha_id;
+
+	public $wecha_user;
 	function _initialize(){
 		parent::_initialize();
 		
@@ -10,6 +12,16 @@ class GiftAction extends WapAction {
 			$this->redirect("Home/Adma/index?token=".$this->token);
 		}
 
+		//检测用户是否已关注
+		$wecha_user = M('WechaUser')->where(array("token"=>$this->token,"wecha_id"=>$this->wecha_id))->find();
+		if( ! $wecha_user)
+		{
+			$this->error("请先关注我们，并从公众号底部菜单进入礼品中心");
+		}
+
+		$this->wecha_user = $wecha_user;
+
+		$this->assign("wecha_user",$wecha_user);
 		$this->assign("token",$this->token);
 		$this->assign("wecha_id",$this->wecha_id);
 	}
@@ -19,8 +31,8 @@ class GiftAction extends WapAction {
 	//活动入口
 	function index(){
 		$list = M('Gift')->select();
-		dump($list);
-		//$this->display();
+		$this->assign("list",$list);
+		$this->display();
 	}
 
 	
